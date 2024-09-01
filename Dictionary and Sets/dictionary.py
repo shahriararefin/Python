7675b799f8aef168616cab8bbba45ff3713a0cce
shahriararefin <shahriararefin7@gmail.com>
2024-09-01 20:10:24 +0600

    dictionary

---
+++ Dictionary and Sets/dictionary.py
@@ -0,0 +1,10 @@
+dic = {
+    "name": "dictionary",
+    "use": "word-meaning",
+    "pages": 1024,
+    "language": "English to Bangla" 
+}
+
+print(dic)
+
+print(dic["name"])
---
+++ set.py
@@ -0,0 +1,15 @@
+set1 = {1 ,3 ,4, 1, 5, 4, 3, "hello", "world", "world", "hello"}
+
+print(set1)
+
+# empty set
+
+collection = set()
+
+print(collection)
+print(type(collection))
+
+collection.add(3)
+collection.add(1)
+
+print(collection)
