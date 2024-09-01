ae673a3ac9aa2b3eb9ed43dd6ba29874b55bd452
shahriararefin <shahriararefin7@gmail.com>
2024-09-01 10:20:34 +0600

    lists in Python

---
+++ String and Conditions/list.py
@@ -0,0 +1,10 @@
+# marks = [73,92,53.6,47.2]
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+
+student = ["Karan", 23, 512, "Dhaka"]
+# print(student[0])
+
+student[0]= "Manoj"
+print(student)
---
+++ String and Conditions/listMethods.py
@@ -0,0 +1,15 @@
+list = [2, 1, 3]
+list.append(4)
+# list.sort()
+list.sort(reverse=True)
+print(list)
+
+
+list.reverse()
+
+list.insert(2, 6)
+
+print(list)
+
+list.remove(2)
+list.pop(1)
