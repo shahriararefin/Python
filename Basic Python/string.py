3a6d281677aad58c5bae09910d4f5d5696f5b82a
Shahriar Arefin <shahriararefin7@gmail.com>
2026-02-11 12:06:38 +0600

    .

---
+++ Basic Python/string.py
@@ -0,0 +1,12 @@
+name = 'Alice'
+name2 = "Bob"
+name3 = """
+        Charlie 
+        Jones
+        Stark
+        """
+        
+        
+print(name3)
+
+print(name.upper())
---
+++ Basic Python/tuples.py
@@ -0,0 +1,9 @@
+def multiple():
+    return 3, 4
+
+# print(multiple())
+
+items = 'pen', 'pencil', 'eraser', 'notebook'
+
+print(items)
+print(type(items))
